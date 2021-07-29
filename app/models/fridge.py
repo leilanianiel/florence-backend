bfffa7ae37764d150530859f08f96055f9717547
Glenda <chicag9313@gmail.com>
2021-07-29 16:08:35 -0700

    made more changes to tables

--- app/models/fridge.py
+++ app/models/fridge.py
@@ -5,4 +5,5 @@ from sqlalchemy.orm import relationship
 # add ForeignKey to connect with item table
 class Fridge(db.Model):
     fridge_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    
     items = db.relationship("Item", backref="fridge")
