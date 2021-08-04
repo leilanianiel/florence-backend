dacd45e3b01f2c27967cdec38c1d8c5a1a3ad044
Brittany <brittanyclemons.info@gmail.com>
2021-08-04 14:31:39 -0700

    add post fridge route

--- app/fridge_routes.py
+++ app/fridge_routes.py
@@ -1,29 +1,32 @@
 from app.models.fridge import Fridge
-from flask import Blueprint, request, jsonify, make_response
-from dotenv import load_dotenv
-import os
-import requests
+from flask import Blueprint, jsonify
 from app import db
-from app.models.item import Item
 from app.models.fridge import Fridge
-from app.models.category import Category
-import datetime
-import json
-from flask_login import login_required 
+from flask_login import login_required
 
 
 # example_bp = Blueprint('example_bp', __name__)
 fridge_bp = Blueprint("fridge", __name__, url_prefix="/fridge")
 
 
+@fridge_bp.route("", methods=["POST"], strict_slashes=False)
+@login_required
+def new_fridge():
+
+    fridge = Fridge()
+    db.session.add(fridge)
+    db.session.commit()
+
+    return jsonify(fridge), 200
+
+
 @fridge_bp.route("", methods=["GET"], strict_slashes=False)
-@login_required 
+@login_required
 def customers_fridge():
-    if request.method == "GET":
 
-        fridge = Fridge.query.all()
-        fridge_response = []
+    fridge = Fridge.query.all()
+    fridge_response = []
 
-        for i in fridge:
-            fridge_response.append(i)
-        return jsonify(fridge_response)
+    for i in fridge:
+        fridge_response.append(i)
+    return jsonify(fridge_response)
