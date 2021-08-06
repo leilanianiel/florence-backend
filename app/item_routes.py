38622dfc7e792fac6fcda25d458e76a5e8030861
Leilani Allen <leilani.allen90@gmail.com>
2021-08-06 16:38:31 -0700

    Delete item when count is zero

--- app/item_routes.py
+++ app/item_routes.py
@@ -94,12 +94,20 @@ def handle_item(id):
 @login_required
 def decrease_count(id):
 
-    
     item = Item.query.get(id)
 
-    item.count -= 1
-    db.session.add(item)
-    db.session.commit()
+    # When count is equal to 0, delete item from fridge 
+    if item.count < 2:
+        item.count -= 1
+        db.session.delete(item)
+        db.session.commit()
+
+        
+    # decrease count of item
+    else: 
+        item.count -= 1
+        db.session.add(item)
+        db.session.commit()
 
     return make_response({"New item count": item.count}, 200)
 
